ff60a1c2b71bb413ab4854177048f399e939748a
Mas Byte <gitlab@masbyte.com>
2024-03-02 10:55:54 -0600

    Traductor con IA

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,38 @@
+import { Form } from 'react-bootstrap'
+import { SectionType } from '../types.d'
+import React from 'react'
+
+interface Props {
+  type: SectionType
+  loading?: boolean
+  onChange: (value: string) => void
+  value: string
+}
+const commonStyles = { border: 0, height: '200px', resize: 'none' }
+
+const getPlaceHolder = ({ type, loading }: { type: SectionType, loading?: boolean }) => {
+  if (type === SectionType.From) return 'Introducir texto'
+  if (loading === true) return 'Traduciendo...'
+  return 'Traducción'
+}
+
+export const TextArea = ({ type, loading, value, onChange }: Props) => {
+  const styles = type === SectionType.From
+    ? commonStyles
+    : { ...commonStyles, backgroundColor: '#f5f5f5' }
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(event.target.value)
+  }
+  return (
+    <Form.Control
+      as='textarea'
+      disabled= {type === SectionType.To }
+      placeholder= {getPlaceHolder({ type, loading })}
+      autoFocus={type === SectionType.From}
+      style= {styles}
+      value={value}
+      onChange={handleChange}
+    />
+  )
+}
---
+++ src/hooks/useStore.ts
@@ -0,0 +1,121 @@
+import { useReducer } from 'react'
+// import { AUTO_LANGUAGE } from '../constants'
+import { type FromLanguage, type Language, type Action, type State } from '../types.d'
+import { AUTO_LANGUAGE } from '../constants'
+
+// 1.create an initialState
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+// 2.create a reducer
+function reducer(state: State, action: Action) {
+  const { type } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    if (state.fromLanguage === AUTO_LANGUAGE) return state
+
+    const loading = state.fromText !== ''
+
+    return {
+      ...state,
+      result: '',
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage,
+      loading
+    }
+  }
+  if (type === 'SET_FROM_LANGUAGE') {
+    if (state.fromLanguage === action.payload) return state
+
+    const loading = state.fromText !== ''
+
+    return {
+      ...state,
+      fromLanguage: action.payload,
+      result: '',
+      loading
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    if (state.toLanguage === action.payload) return state
+    const loading = state.fromText !== ''
+
+    return {
+      ...state,
+      toLanguage: action.payload,
+      result: '',
+      loading
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    const loading = action.payload !== ''
+
+    return {
+      ...state,
+      loading,
+      result: action.payload
+    }
+  }
+  return state
+}
+
+export function useStore() {
+  // 3.Usar el hookReducer
+  const [{
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading
+  }, dispatch] = useReducer(reducer, initialState)
+
+  const interchangeLanguages = () => {
+    dispatch({ type: 'INTERCHANGE_LANGUAGES' })
+  }
+
+  const setFromLanguage = (payload: FromLanguage) => {
+    dispatch({ type: 'SET_FROM_LANGUAGE', payload })
+    console.log(payload)
+  }
+
+  const setToLanguage = (payload: Language) => {
+    dispatch({ type: 'SET_TO_LANGUAGE', payload })
+  }
+
+  const setFromText = (payload: string) => {
+    dispatch({ type: 'SET_FROM_TEXT', payload })
+  }
+
+  const setResult = (payload: string) => {
+    dispatch({ type: 'SET_RESULT', payload })
+  }
+
+  return {
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage,
+    setFromText,
+    setResult
+  }
+}
